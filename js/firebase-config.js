92ec9e9b9dc7e6b3243385c4602f2fd0af8f3d4d
Raket5 <mahmud.bnschool@gmail.com>
2026-02-14 13:35:20 +0600

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration object with project settings.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,10 @@
-
+// Apnar Firebase configuration (Project settings theke copy korun)
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://bnschool-mgmt-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
